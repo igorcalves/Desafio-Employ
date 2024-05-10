59247f318cef69cd4216ffe206a553e63304f18d
Igor Alves <igor193@live.com>
2024-05-10 18:34:27 -0300

    v1

---
+++ pages/script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.botaoAssinar').forEach(function (e) {
+    e.addEventListener('click', function () {
+        window.location.href = '/pages/telaPagamento.html'
+    });
+})
+
+
+
+const inicio = document.querySelector("[inicio]");
+
+inicio.addEventListener('click', _ => {
+    inicio.href = "../index.html";
+})
+
